cd8c72965095e38e2ae39d9de94a36f1f43a68b2
zhuy-nguyenz <graio911@gmail.com>
2021-06-03 10:01:49 +0700

    '2nd'

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-function oneVariableEquation(a,b){
-    let equationVariable
-    if (a == 0) {
-        if (b == 0) {
+var a = 5
+var b = 8
+
+function oneVariableEquation(numA, numB) {
+    let equationVariable = 0
+    if (numA == 0) {
+        if (numB == 0) {
             equationVariable = "Phương trình vô số nghiệm"
-        }
-        else if (b != 0) {
+        } else if (numB != 0) {
             equationVariable = "Phương trình vô nghiệm"
         }
-    }
-    else if (a != 0) {
-        equationVariable = -b / a
+    } else if (numA != 0) {
+        equationVariable = (-numB / numA)
     }
     return equationVariable
 }
 
-document.write("Đáp án: " + oneVariableEquation(5,8))
+console.log(oneVariableEquation(a, b))
